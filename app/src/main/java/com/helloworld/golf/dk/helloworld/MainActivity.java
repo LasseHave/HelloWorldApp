045042b9c329bcf632ff12b945fce8fd389356a1
Benjamin Schlie <bschlie@adslhome.dk>
2017-10-25 21:18:06 +0200

    Adding Accelerometer measurement (just displaying event values x, y, z)

--- app/src/main/java/com/helloworld/golf/dk/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/golf/dk/helloworld/MainActivity.java
@@ -2,16 +2,28 @@ package com.helloworld.golf.dk.helloworld;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private CameraManager camManager;
     private boolean flashOn = false;
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    private TextView textView_x;
+    private TextView textView_y;
+    private TextView textView_z;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,30 @@ public class MainActivity extends AppCompatActivity {
         if (this.getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
             setupFlashButton();
         }
+
+        initAccelerometer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void initAccelerometer() {
+        textView_x = (TextView) findViewById(R.id.activity_main_sensor_x);
+        textView_y = (TextView) findViewById(R.id.activity_main_sensor_y);
+        textView_z = (TextView) findViewById(R.id.activity_main_sensor_z);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     private void setupFlashButton() {
@@ -42,5 +78,21 @@ public class MainActivity extends AppCompatActivity {
         flashOn = !flashOn;
         String cameraId = camManager.getCameraIdList()[0]; // Usually front camera is at 0 position.
         camManager.setTorchMode(cameraId, flashOn);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            textView_x.setText(Float.toString(event.values[0]));
+            textView_y.setText(Float.toString(event.values[1]));
+            textView_z.setText(Float.toString(event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
