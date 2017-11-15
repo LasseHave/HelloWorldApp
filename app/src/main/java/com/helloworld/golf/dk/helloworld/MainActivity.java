50268df7089a811f36a0fcfc34e68f61732edea3
Lasse Have Nielsen <lasse@havenielsen.dk>
2017-11-15 12:37:02 +0100

    added two new activities to use in the app. Calibration and Start Acktivity

--- app/src/main/java/com/helloworld/golf/dk/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/golf/dk/helloworld/MainActivity.java
@@ -1,13 +1,12 @@
 package com.helloworld.golf.dk.helloworld;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,8 +14,6 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private CameraManager camManager;
-    private boolean flashOn = false;
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
 
@@ -30,9 +27,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (this.getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-            setupFlashButton();
-        }
+        Button next = (Button) findViewById(R.id.activity_main_calibrate_btn);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Calibrate.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
 
         initAccelerometer();
     }
@@ -59,25 +61,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    private void setupFlashButton() {
-        final Button flashBtn = (Button) findViewById(R.id.activity_main_flash_btn);
-        camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
-        flashBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    toggleFlash();
-                } catch (CameraAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void toggleFlash() throws CameraAccessException {
-        flashOn = !flashOn;
-        String cameraId = camManager.getCameraIdList()[0]; // Usually front camera is at 0 position.
-        camManager.setTorchMode(cameraId, flashOn);
+    private void calibrate() {
+        /* new acktivity here */
     }
 
     @Override
