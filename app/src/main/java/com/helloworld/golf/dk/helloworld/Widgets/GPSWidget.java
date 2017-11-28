5cae6c4940b2a03457c50bdf4b9363226af6c793
Lasse Have Nielsen <201370981@iha.dk>
2017-11-28 11:37:44 +0100

    Added GPS Widget.
    To noisy to do in class

---
+++ app/src/main/java/com/helloworld/golf/dk/helloworld/Widgets/GPSWidget.java
@@ -0,0 +1,15 @@
+package com.helloworld.golf.dk.helloworld.Widgets;
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import com.helloworld.golf.dk.helloworld.Models.Acceleration;
+
+
+public class GPSWidget implements SensorEventListener {
+    public GPSWidget(Activity activity) {
+
+    }
+}
